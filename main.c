b4df2559970483ad4313cbe566be169624f8a881
MORAZ Sebastien <Sebastien.MORAZ@cpnv.ch>
2020-03-04 12:16:27 +0100

    base du C

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include <stdlib.h>
+
+
+void Title();
+void Home();
+
+char username[24];
+char password[24];
+
 
 int main() {
-    printf("Hello, World!\n");
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleTitle("Bataille Navale");
+    Home();
+    system("pause");
     return 0;
+}
+
+void Title(){
+    printf("--------------\n");
+    printf("Bataille Naval\n");
+    printf("--------------\n");
+}
+
+void Home(){
+    Title();
+    printf("By Sébastien Moraz\n\n");
+
 }
